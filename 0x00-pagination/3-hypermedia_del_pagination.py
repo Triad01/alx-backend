7b15685019aab97d02312d767c337450d28f77b0
triad01 <luckypee01@gmail.com>
2024-08-18 12:04:24 +0100

    fixed task requirement

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -34,7 +34,9 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict[str, Any]:
+    def get_hyper_index(self,
+                        index: int = None,
+                        page_size: int = 10) -> Dict[str, Any]:
         """
         Return a dict containing pagination details with
         resilience to deletions.
